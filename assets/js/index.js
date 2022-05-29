f7c1bd32b2da14d581c652ba1a2c5b7456d6103e
Luiz Felipe Sacardo <luizfelipe_1992@hotmail.com>
2022-05-28 21:43:07 -0300

    Second Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,9 @@
-let button = document.querySelector('#button');
-let value01 = document.querySelector('#value01').value;
-let value02 = document.querySelector('#value02').value;
-let tax01 = document.querySelector('#tax01').value;
-let tax02 = document.querySelector('#tax02').value;
-let periods01 = document.querySelector('#periods01').value;
-let periods02 = document.querySelector('#periods02').value;
-let periodType01 = document.querySelector('#periodType01').value;
-let periodType02 = document.querySelector('#periodType02').value;
-let selic = 12.75;
 let results = document.querySelector('.results');
-
+let button = document.querySelector('#button');
 button.addEventListener('click', generateResults);
 
 
-function teste(){
-  event.preventDefault();
-  let x = valuesPercentagesOverTotal();
-  console.log(x)
-}
-
-
-function convertPeriodsToDays(){
+function convertPeriodsToDays(periods01, periods02, periodType01, periodType02){
   let periods = [];
 
   let firstPeriod = periods01 * periodType01;
@@ -32,7 +15,7 @@ function convertPeriodsToDays(){
   return periods;
 }
 
-function valuesPercentagesOverTotal(){
+function valuesPercentagesOverTotal(value01, value02){
   let value1 = value01;  
   let value2 = value02;
   let totalValue = Number(value1) + Number(value2);
@@ -43,62 +26,101 @@ function valuesPercentagesOverTotal(){
   return [value1Percentage, value2Percentage]
 }
 
-function ponderateTaxes(){
-  let tax1 = tax01;
-  let tax2 = tax02;
-
-  let ponderatedTax01 = valuesPercentagesOverTotal()[0]*tax1;
-  let ponderatedTax02 = valuesPercentagesOverTotal()[1]*tax2;
+function ponderateTaxes(tax01, tax02, value01, value02){
+  let ponderatedTax01 = valuesPercentagesOverTotal(value01, value02)[0]*tax01;
+  let ponderatedTax02 = valuesPercentagesOverTotal(value01, value02)[1]*tax02;
 
   let ponderatedTax = ponderatedTax01 + ponderatedTax02;
 
   return ponderatedTax;
 }
 
-function periodTotalTax(){
-  let days = convertPeriodsToDays();
+function periodTotalTax(tax01, tax02, periods01, periods02, periodType01, periodType02, value01, value02){
+  let days = convertPeriodsToDays(periods01, periods02, periodType01, periodType02);
   let days1 = days[0];
   let days2 = days[1];
-  let tax1 = tax01;
-  let tax2 = tax02;
-  let value01Percentage = valuesPercentagesOverTotal()[0];
-  let value02Percentage = valuesPercentagesOverTotal()[1];
+
+  let value01Percentage = valuesPercentagesOverTotal(value01, value02)[0];
+  let value02Percentage = valuesPercentagesOverTotal(value01, value02)[1];
 
   let days1Factor = (days1/360);
   let days2Factor = (days2/360);
 
-  let totalTax01 = days1Factor*tax1*value01Percentage;
-  let totalTax02 = days2Factor*tax2*value02Percentage;
+  let totalTax01 = days1Factor*tax01*value01Percentage;
+  let totalTax02 = days2Factor*tax02*value02Percentage;
 
   let totalPeriodTax = totalTax01+totalTax02;
 
   return totalPeriodTax;
 }
 
-function taxOverSelic(){
-  let efetiveTax = ponderateTaxes();
+function taxOverSelic(tax01, tax02, selic, value01, value02){
+  let efetiveTax = ponderateTaxes(tax01, tax02, value01, value02);
   return efetiveTax/selic;
 }
 
 function generateResults(){
   event.preventDefault();
   results.classList.remove('hide');
-  button.classList.add('hide')
-  let ponderateTax = ponderateTaxes();
-  let totalTax = periodTotalTax();
-  let valuePercentage01 = valuesPercentagesOverTotal()[0]*100;
-  let valuePercentage02 = valuesPercentagesOverTotal()[1]*100;
-  let taxOverSelicPercentage = taxOverSelic()*100;
+  button.classList.add('hide');
 
-  results.innerHTML = `A taxa MIX anual é de <span class="results__output--highlight-main">${ponderateTax.toFixed(2)}%</span>, o que representa <span class="results__output--highlight">${taxOverSelicPercentage.toFixed(0)}%</span> da taxa Selic atual.<br> A taxa total do período é de <span class="results__output--highlight"> ${totalTax.toFixed(2)}%.</span> <br> Esta simulação apresenta <span class="results__output--highlight"> ${valuePercentage01.toFixed(2)}% </span> de Recursos controlados e <span class="results__output--highlight"> ${valuePercentage02.toFixed(2)}% </span> de recursos livres. <button type="submit" class="form__button" id="redoButton">Refazer</button>`
+  let errorsList = [];
+  
+  let selic = 12.75;
+  let tax01Input = document.querySelector('#tax01').value;
+  let tax01 = tax01Input.replace(',', '.');
+  let tax02Input = document.querySelector('#tax02').value;
+  let tax02 = tax02Input.replace(',', '.');
+  let value01Input = document.querySelector('#value01').value;
+  let value01 = value01Input.replace(',', '.');
+  let value02Input = document.querySelector('#value02').value;
+  let value02 = value02Input.replace(',', '.');
+  let periods01Input = document.querySelector('#periods01').value;
+  let periods01 = periods01Input.replace(',', '.');
+  let periods02Input = document.querySelector('#periods02').value;
+  let periods02 = periods02Input.replace(',', '.');
+  let periodType01 = document.querySelector('#periodType01').value;
+  let periodType02 = document.querySelector('#periodType02').value;
 
-  window.scrollBy({
-    top: 2000,
-    behavior: 'smooth'
-  });
+  let ponderateTax = ponderateTaxes(tax01, tax02, value01, value02);
+  let totalTax = periodTotalTax(tax01, tax02, periods01, periods02, periodType01, periodType02, value01, value02);
+  let valuePercentage01 = valuesPercentagesOverTotal(value01, value02)[0]*100;
+  let valuePercentage02 = valuesPercentagesOverTotal(value01, value02)[1]*100;
+  let taxOverSelicPercentage = taxOverSelic(tax01, tax02, selic, value01, value02)*100;
 
-  let redoButton = document.querySelector('#redoButton');
-  redoButton.addEventListener('click', redoSimulation);
+  errorsList = formValidation(tax01, tax02, periods01, periods02, value01, value02);
+
+
+  if(errorsList.length <=0){
+    results.innerHTML = `A taxa MIX anual é de <span class="results__output--highlight-main">${ponderateTax.toFixed(2)}%</span>, o que representa <span class="results__output--highlight">${taxOverSelicPercentage.toFixed(0)}%</span> da taxa Selic atual.<br> <br> A taxa total do período é de <span class="results__output--highlight"> ${totalTax.toFixed(2)}%.</span> <br> <br> Esta simulação apresenta: <br> <span class="results__output--highlight"> ${valuePercentage01.toFixed(2)}% </span> de Recursos Controlados <br> <span class="results__output--highlight"> ${valuePercentage02.toFixed(2)}% </span> de Recursos Livres <button type="submit" class="form__button" id="redoButton">Refazer</button>`
+    
+    let redoButton = document.querySelector('#redoButton');
+    redoButton.addEventListener('click', redoSimulation); 
+   
+   window.scrollBy({
+     top: 2000,
+     behavior: 'smooth'
+   });
+  } else{
+    let outputString = `<span class="results__error-title">Atenção, existem erros de preenchimento:</span> <br> <br>`;
+
+    errorsList.forEach(element => {
+      let newElement =  `<span class="results__error-simbol">&#10008</span> <span class="results__error-element"> ${element}</span> <br>`;
+      outputString+=newElement;      
+    });
+
+    let outputHtml = outputString+`<button type="submit" class="form__button" id="redoButton">Refazer</button>`;
+
+    results.innerHTML = outputHtml;
+
+    let redoButton = document.querySelector('#redoButton');
+    redoButton.addEventListener('click', redoSimulation);
+
+    window.scrollBy({
+      top: 2000,
+      behavior: 'smooth'
+    });
+  }
 }
 
 function redoSimulation(){
@@ -109,5 +131,41 @@ function redoSimulation(){
     top: -100,
     behavior: 'smooth'
   });
+}
+
+function formValidation(tax01, tax02, periods01, periods02, value01, value02){
+  let errors = [];
+
+  if(tax01.length < 1){
+    let errorDescription = "Digite uma TAXA VÁLIDA em Resursos Controlados";
+    errors.push(errorDescription);
+  }
+
+  if(tax02.length < 1){
+    let errorDescription = "Digite uma TAXA VÁLIDA em Recursos Livres";
+    errors.push(errorDescription);
+  }
+  
+  if(periods01.length < 1){
+    let errorDescription = "Digite um PRAZO VÁLIDO em Recursos Obrigatorios";
+    errors.push(errorDescription);
+  }
+
+  if(periods02.length < 1){
+    let errorDescription = "Digite um PRAZO VÁLIDO em Recursos Livres";
+    errors.push(errorDescription);
+  }
+
+  if(value01.length < 1){
+    let errorDescription = "Digite um VALOR VÁLIDO em Recursos Obrigatorios";
+    errors.push(errorDescription);
+  }
+
+  if(value02.length < 1){
+    let errorDescription = "Digite um VALOR VÁLIDO em Recursos Livres";
+    errors.push(errorDescription);
+  }
+
+  return errors;
 }
 
